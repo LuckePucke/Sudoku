9972149a6c37f2954a6359c8a84237b1850afcda
Lucas Bäckvall <lucas.backvall@gmail.com>
2014-09-25 11:15:32 +0200

    Updated gitignore and competion of assignment A.

--- Sudoku.hs
+++ Sudoku.hs
@@ -10,6 +10,34 @@ data Sudoku = Sudoku [[Maybe Int]]
 rows :: Sudoku -> [[Maybe Int]]
 rows (Sudoku rs) = rs
 
+example :: Sudoku
+example = Sudoku
+    [ 
+        [Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing],
+        [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing],
+        [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing],
+        [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8],
+        [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9],
+        [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing],
+        [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing],
+        [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing],
+        [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+    ]
+
+exampleFalse :: Sudoku
+exampleFalse = Sudoku
+    [ 
+        [Just 10, Just 0, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing],
+        [Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing],
+        [Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing],
+        [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8],
+        [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9],
+        [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing],
+        [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing],
+        [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing],
+        [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
+    ]
+
 -- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku (replicate 9 row)
@@ -18,14 +46,33 @@ allBlankSudoku = Sudoku (replicate 9 row)
 -- isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku s = and [and b | b = [length (rows s) == 9, length r == 9]]
-    where 
-        r = [row | row <- (rows s)]
-        p = [pos | pos <- r]
+isSudoku s = and [
+                  b | r <- [row | row <- (rows s)],
+                      p <- [pos | pos <- r],
+                      b <- [
+                            length (rows s) == 9,
+                            length r == 9,
+                            isOK p
+                           ]
+                 ]
+
+isOK :: Maybe Int -> Bool
+isOK Nothing = True
+isOK (Just n) | n < 1     = False
+              | n > 9     = False
+              | otherwise = True
+
+isNotNothing :: Maybe Int -> Bool
+isNotNothing Nothing = False
+isNotNothing _       = True
 
 -- isSolved sud checks if sud is already solved, i.e. there are no blanks
 isSolved :: Sudoku -> Bool
-isSolved = undefined
+isSolved s = and [
+                  b | r <- [row | row <- (rows s)],
+                      p <- [pos | pos <- r],
+                      b <- [isNotNothing p]
+                 ]
 
 -------------------------------------------------------------------------
 
